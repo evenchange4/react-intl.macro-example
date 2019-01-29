1558de1126da608adc464059f97c1ec9ea5c8dd3
Michael Hsu (徐承志) <evenchange4@gmail.com>
2019-01-29 14:57:10 +0800

    update

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+// @flow
+import * as React from 'react';
+import { FormattedMessage } from 'react-intl.macro';
+
+const Body = () => (
+  <FormattedMessage
+    id="Body.text"
+    defaultMessage="Body text"
+    description="Body text"
+  />
+);
+
+export default Body;
